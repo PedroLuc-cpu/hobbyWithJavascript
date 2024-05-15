d8891a943ffb55e8c1dc42a5ccf4eff28bf5f7d5
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2024-05-15 16:22:14 -0300

    feature: personalizado mensagem quando não produto cadastrado.

--- assets/scripts/array.js
+++ assets/scripts/array.js
@@ -83,6 +83,9 @@ button_cadastrar_produto.addEventListener("click", cadastrarProduto)
 function rederizarCarrinhoDeCompras() {
   if(carrinhoDeCompras.length === 0) {
     listOfBuying.innerHTML = "<li>Nenhum produto cadastrado</li>";
+    listOfBuying.style.textAlign = "center";
+    listOfBuying.style.fontSize = "2rem"
+    listOfBuying.style.color = "#1B1B1B"
     return;
   }
 
