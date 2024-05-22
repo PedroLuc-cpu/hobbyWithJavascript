9c7ca90a9bd8fdc39f26e504e9d4508b811edfa7
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2024-05-22 17:20:18 -0300

    feature: implementando a forma de pagamento

---
+++ assets/scripts/pagamento.js
@@ -0,0 +1,21 @@
+const forma_de_pagamento_dinherio = document.getElementById(
+  "forma_de_pagamento_dinherio"
+);
+
+const forma_de_pagamento_cartao = document.getElementById(
+  "forma_de_pagamento_cartao"
+);
+
+const forma_de_pagamento_pix = document.getElementById(
+  "forma_de_pagamento_pix"
+);
+
+const forma_de_pagamento_loja = document.getElementById(
+  "forma_de_pagamento_loja"
+);
+
+const pagamento_no_credito = document.getElementById("pagamento_no_credito");
+
+pagamento_no_credito.addEventListener("click", () => {
+  console.log("pagamento_no_no")
+})
